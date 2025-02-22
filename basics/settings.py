9fe7e30e963522fe24bcd25fe8a64d4804c0c343
jhonatanmizu <natanjesuss20@gmail.com>
2025-02-22 18:30:15 -0300

    feat: create global templates

--- basics/settings.py
+++ basics/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'basics.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'common'
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
