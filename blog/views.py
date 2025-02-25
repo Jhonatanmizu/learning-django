a064ef75a492e6177ef545d0a67a9b860b1e6203
jhonatanmizu <natanjesuss20@gmail.com>
2025-02-25 00:54:53 -0300

    feat: create posts with dynamic data

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render # type: ignore
 from django.http import HttpResponse # type: ignore
-
+from .data import posts
 
 # Create your views here.
 def index(request):
-  return render(request, 'blog/index.html')
+  context={
+    "document_title":"Blog",
+    "title":"Blog Page",
+    "description":"This is the blog page",
+    "posts":posts
+  }
+  return render(request, 'blog/index.html',context)
